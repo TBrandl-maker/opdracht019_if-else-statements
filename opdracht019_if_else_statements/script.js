4ecba7960436e7cca34ea6bf630cb08d5c4428c1
TBrandl <58363852+TBrandl-maker@users.noreply.github.com>
2020-09-07 15:14:34 +0200

    Add files via upload

---
+++ opdracht019_if_else_statements/script.js
@@ -0,0 +1,34 @@
+const age = 20;
+
+if (age > 18) {
+
+    document.write("welcome to our bar");
+
+} else {
+
+    document.write("you are not allowed to enter the bar");
+
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    document.write("welcome, my lady");
+
+} else {
+
+    document.write("Tonight is ladiesnight, please leave")
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+
+    document.write("you are allowed to drive")
+
+} else {
+    document.write("you are not allowed to drive")
+}
+
+
